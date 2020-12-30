f464a19af0009151e39f1feb299650bc9ae356f6
张明爽 <mingshuang.zhang@nascent.cn>
2020-12-30 11:33:24 +0800

    test(core): add tests.

---
+++ magneton-core/src/test/java/org/magneton/core/util/MoreCollectionsTest.java
@@ -0,0 +1,22 @@
+package org.magneton.core.util;
+
+import java.util.Collections;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * .
+ *
+ * @author zhangmsh
+ * @version 1.0.0
+ * @since 2020/12/30
+ */
+class MoreCollectionsTest {
+
+  @Test
+  void isNullOrEmpty() {
+    Assertions.assertTrue(MoreCollections.isNullOrEmpty(Collections.emptyList()), "empty list");
+    Assertions.assertTrue(MoreCollections.isNullOrEmpty(Collections.emptyMap()), "empty map");
+    Assertions.assertFalse(MoreCollections.isNullOrEmpty(Collections.singletonList("1")), "not empty");
+  }
+}
