a3b5dc9297bc5b0ba7e08d8b5c49f3fadbc3aefb
张明爽 <mingshuang.zhang@nascent.cn>
2020-12-31 13:56:23 +0800

    refactor(aes): change mode to GCM
    GCM is the better encryption mode.

--- magneton-core/src/main/java/org/magneton/core/security/Aes.java
+++ magneton-core/src/main/java/org/magneton/core/security/Aes.java
@@ -2,298 +2,160 @@ package org.magneton.core.security;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import javax.annotation.Nullable;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.GCMParameterSpec;
 import org.magneton.core.util.Bytes;
 
 /**
- * AES utils.
- *
- * <p>Note:AES is not a highly security algorithm.
+ * Aes Utils.
  *
  * @author zhangmsh
- * @since 2019-04-27
+ * @version 1.0.0
+ * @since 2020/12/31
  */
-@SuppressWarnings({"ALL", "java:S112", "java:S5542"})
 public class Aes {
 
-  public static final int LEGAL_AES_KEYSIZES = 16;
-
-  /** algorithm/model/padding. */
-  private static final String CIPHERMODE = "AES/CBC/PKCS5Padding";
-  /** algorithm/model/padding. */
-  private static final String CIPHERMODE_NOPADDING = "AES/CBC/NOPadding";
-
-  private static final char PADDING = 'x';
+  public static final int GCM_TAG_LENGTH = 16;
+  public static final int GCM_IV_LENGTH = 12;
+  private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+  private static final int KEY_SIZE = 128;
+  private static final String AES_GCM_NO_PADDING = "AES/GCM/NoPadding";
 
   private Aes() {}
 
   /**
-   * convert the key to legal size.
+   * generate secret key.
    *
-   * @param key decode/encode key.
-   * @return the legal key bytes.
+   * @param password the given password.
+   * @return the secret key.
    */
-  public static byte[] getBytesWithLegalSize(String key) {
-    return getBytesWithLegalSize(key, StandardCharsets.UTF_8);
-  }
-
-  /**
-   * convert the key to legal size.
-   *
-   * @param key decode/encode key.
-   * @param charset the specity charset.
-   * @return the legal key bytes.
-   */
-  public static byte[] getBytesWithLegalSize(String key, Charset charset) {
-    int len = key.length();
-    byte[] bytes = key.getBytes(charset);
-    if (len != LEGAL_AES_KEYSIZES) {
-      bytes = Arrays.copyOf(bytes, LEGAL_AES_KEYSIZES);
-      // data padding.
-      for (int i = len; i < LEGAL_AES_KEYSIZES; ++i) {
-        bytes[i] = PADDING;
+  public static SecretKey generateKey(@Nullable String password) {
+    try {
+      KeyGenerator kgen = KeyGenerator.getInstance("AES");
+      if (password == null || password.isEmpty()) {
+        kgen.init(KEY_SIZE);
+      } else {
+        kgen.init(KEY_SIZE, new SecureRandom(password.getBytes(DEFAULT_CHARSET)));
       }
+      return kgen.generateKey();
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalStateException(e.toString());
     }
-    return bytes;
-  }
-  /**
-   * AES-128/CBC encrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String aes128CBCEncrypt(String content, String password, String iv)
-      throws Exception {
-    return aes128CBCEncrypt(content, password, iv, StandardCharsets.UTF_8);
   }
 
   /**
-   * AES-128/CBC encrypt.
+   * generate secret key.
    *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @param charset the given content charset.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
+   * @return the secret key.
    */
-  public static String aes128CBCEncrypt(String content, String password, String iv, Charset charset)
-      throws Exception {
-    Cipher cipher = Cipher.getInstance(CIPHERMODE);
-    cipher.init(Cipher.ENCRYPT_MODE, createSecretKeySpec(password), createIvParameterSpec(iv));
-    byte[] result = cipher.doFinal(content.getBytes(charset));
-    return Bytes.bytes2hex(result);
+  public static SecretKey generateKey() {
+    return generateKey(null);
   }
 
   /**
-   * AES-128/CBC no padding encrypt.
+   * encrypt the given plaintext.
    *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
+   * @param password the given password.
+   * @param plaintext the given plaintext to encrypt.
+   * @return the encrypted text.
+   * @throws IllegalStateException if skey is not valid and GCM mode is not available in the JRE.
    */
-  public static String aesCBCNoPaddingEncrypt(String content, String password, String iv)
-      throws Exception {
-    return aesCBCNoPaddingEncrypt(content, password, iv, StandardCharsets.UTF_8);
+  public static String encrypt(String password, String plaintext) {
+    return encrypt(generateKey(password), plaintext);
   }
+
   /**
-   * AES-128/CBC no padding encrypt.
+   * encrypt the given plaintext.
    *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @param charset the given content charset.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
+   * @param skey secret key.
+   * @param plaintext the given plaintext to encrypt.
+   * @return the ciphertext.
+   * @throws IllegalStateException if skey is not valid and GCM mode is not available in the JRE
    */
-  public static String aesCBCNoPaddingEncrypt(
-      String content, String password, String iv, Charset charset) throws Exception {
-    Cipher cipher = Cipher.getInstance(CIPHERMODE_NOPADDING);
-    cipher.init(Cipher.ENCRYPT_MODE, createSecretKeySpec(password), createIvParameterSpec(iv));
-    byte[] bytes = content.getBytes(charset);
-    int plaintextLength = bytes.length;
-    plaintextLength = nextSize(plaintextLength < 16 ? 15 : plaintextLength - 1);
-    if (plaintextLength > bytes.length) {
-      bytes = Arrays.copyOf(bytes, plaintextLength);
+  public static String encrypt(SecretKey skey, String plaintext) {
+    try {
+      Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
+      byte[] initVector = new byte[GCM_IV_LENGTH];
+      (new SecureRandom()).nextBytes(initVector);
+      GCMParameterSpec spec =
+          new GCMParameterSpec(GCM_TAG_LENGTH * java.lang.Byte.SIZE, initVector);
+      cipher.init(Cipher.ENCRYPT_MODE, skey, spec);
+      byte[] encoded = plaintext.getBytes(DEFAULT_CHARSET);
+      int ivLength = initVector.length;
+      byte[] ciphertext = new byte[ivLength + cipher.getOutputSize(encoded.length)];
+      System.arraycopy(initVector, 0, ciphertext, 0, ivLength);
+      // Perform encryption
+      cipher.doFinal(encoded, 0, encoded.length, ciphertext, initVector.length);
+      return Bytes.bytes2hex(ciphertext);
+    } catch (NoSuchPaddingException
+        | InvalidAlgorithmParameterException
+        | ShortBufferException
+        | BadPaddingException
+        | IllegalBlockSizeException
+        | InvalidKeyException
+        | NoSuchAlgorithmException e) {
+      /* None of these exceptions should be possible if precond is met. */
+      throw new IllegalStateException(e.toString());
     }
-    byte[] result = cipher.doFinal(bytes);
-    return Bytes.bytes2hex(result);
   }
+
   /**
-   * AES-128/CBC decrypt.
+   * decrypt the given ciphertext.
    *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @return the decrypt data.
-   * @throws Exception if is bad.
+   * @param password the given password.
+   * @param ciphertext the given chiphertext.
+   * @return the plaintext.
+   * @throws BadPaddingException if these indicate corrupt or malicious ciphertext. Note that
+   *     AEADBadTagException may be thrown in GCM mode; this is a subclass of BadPaddingException.
+   * @throws IllegalBlockSizeException if these indicate corrupt or malicious ciphertext.
+   * @throws IllegalStateException if skey is not valid and GCM mode is not available in the JRE.
    */
-  public static String aes128CBCDecrypt(String content, String password, String iv)
-      throws Exception {
-    return aes128CBCDecrypt(content, password, iv, StandardCharsets.UTF_8);
+  public static String decrypt(String password, String ciphertext)
+      throws BadPaddingException, IllegalBlockSizeException {
+    return decrypt(generateKey(password), ciphertext);
   }
+
   /**
-   * AES-128/CBC decrypt.
+   * decrypt the given ciphertext.
    *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @param charset the given content charset.
-   * @return the decrypt data.
-   * @throws Exception if is bad.
+   * @param skey the secret key.
+   * @param ciphertext the given chiphertext.
+   * @return the plaintext.
+   * @throws BadPaddingException if these indicate corrupt or malicious ciphertext. Note that
+   *     AEADBadTagException may be thrown in GCM mode; this is a subclass of BadPaddingException.
+   * @throws IllegalBlockSizeException if these indicate corrupt or malicious ciphertext.
+   * @throws IllegalStateException if skey is not valid and GCM mode is not available in the JRE.
    */
-  public static String aes128CBCDecrypt(String content, String password, String iv, Charset charset)
-      throws Exception {
-    Cipher cipher = Cipher.getInstance(CIPHERMODE);
-    cipher.init(Cipher.DECRYPT_MODE, createSecretKeySpec(password), createIvParameterSpec(iv));
-    byte[] bytes = Bytes.hex2bytes(content);
-    byte[] result = cipher.doFinal(bytes);
-    return new String(result, charset);
-  }
-  /**
-   * AES-128/CBC no padding decrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @return the decrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String aesCBCNoPaddingDecrypt(String content, String password, String iv)
-      throws Exception {
-    return aesCBCNoPaddingDecrypt(content, password, iv, StandardCharsets.UTF_8);
-  }
-  /**
-   * AES-128/CBC no padding decrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param iv initial vector.
-   * @param charset the given content charset.
-   * @return the decrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String aesCBCNoPaddingDecrypt(
-      String content, String password, String iv, Charset charset) throws Exception {
-    Cipher cipher = Cipher.getInstance(CIPHERMODE_NOPADDING);
-    cipher.init(Cipher.DECRYPT_MODE, createSecretKeySpec(password), createIvParameterSpec(iv));
-    byte[] bytes = Bytes.hex2bytes(content);
-    byte[] result = cipher.doFinal(bytes);
-    int binary = result.length >> 2;
-    byte bb = result[binary];
-    if (bb == 0) {
-      while (bb == 0) {
-        bb = result[--binary];
-      }
-    } else {
-      while (bb != 0) {
-        bb = result[++binary];
-      }
-      --binary;
+  public static String decrypt(SecretKey skey, String ciphertext)
+      throws BadPaddingException, IllegalBlockSizeException {
+    try {
+      byte[] cipherBytes = Bytes.hex2bytes(ciphertext);
+      Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
+      byte[] initVector = Arrays.copyOfRange(cipherBytes, 0, GCM_IV_LENGTH);
+      GCMParameterSpec spec =
+          new GCMParameterSpec(GCM_TAG_LENGTH * java.lang.Byte.SIZE, initVector);
+      cipher.init(Cipher.DECRYPT_MODE, skey, spec);
+      byte[] plaintext =
+          cipher.doFinal(cipherBytes, GCM_IV_LENGTH, cipherBytes.length - GCM_IV_LENGTH);
+      return new String(plaintext, DEFAULT_CHARSET);
+    } catch (NoSuchPaddingException
+        | InvalidAlgorithmParameterException
+        | InvalidKeyException
+        | NoSuchAlgorithmException e) {
+      /* None of these exceptions should be possible if precond is met. */
+      throw new IllegalStateException(e.toString());
     }
-    return new String(result, 0, binary + 1, charset);
-  }
-  /**
-   * encrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String encrypt(String content, String password) throws Exception {
-    return encrypt(content, password, StandardCharsets.UTF_8);
-  }
-  /**
-   * encrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param charset the given content charset.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String encrypt(String content, String password, Charset charset) throws Exception {
-    KeyGenerator kgen = KeyGenerator.getInstance("AES");
-    kgen.init(128, new SecureRandom(password.getBytes(charset)));
-    byte[] bytes = kgen.generateKey().getEncoded();
-    Cipher cipher = Cipher.getInstance("AES");
-    cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(bytes, "AES"));
-    byte[] result = cipher.doFinal(content.getBytes(charset));
-    return Bytes.bytes2hex(result);
-  }
-
-  /**
-   * decrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String decrypt(String content, String password) throws Exception {
-    return decrypt(content, password, StandardCharsets.UTF_8);
-  }
-  /**
-   * decrypt.
-   *
-   * @param content the given encrypting content.
-   * @param password the encrypting content password.
-   * @param charset the given content charset.
-   * @return the encrypt data.
-   * @throws Exception if is bad.
-   */
-  public static String decrypt(String content, String password, Charset charset) throws Exception {
-    byte[] bytes = Bytes.hex2bytes(content);
-    KeyGenerator kgen = KeyGenerator.getInstance("AES");
-    kgen.init(128, new SecureRandom(password.getBytes()));
-    SecretKey secretKey = kgen.generateKey();
-    SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-    Cipher cipher = Cipher.getInstance("AES");
-    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-    byte[] result = cipher.doFinal(bytes);
-    return new String(result, charset);
-  }
-
-  private static int nextSize(int i) {
-    int newCapacity = i;
-    newCapacity |= newCapacity >>> 1;
-    newCapacity |= newCapacity >>> 2;
-    newCapacity |= newCapacity >>> 4;
-    newCapacity |= newCapacity >>> 8;
-    newCapacity |= newCapacity >>> 16;
-    newCapacity++;
-    return newCapacity;
-  }
-
-  /**
-   * create secret key spec.
-   *
-   * @param key encode key.
-   * @return SecretKeySpec
-   */
-  private static SecretKeySpec createSecretKeySpec(String key) {
-    return new SecretKeySpec(getBytesWithLegalSize(key), "AES");
-  }
-
-  /**
-   * create initial vector paramter spec.
-   *
-   * @param iv initial vecto.
-   * @return IvParameterSpec
-   */
-  private static IvParameterSpec createIvParameterSpec(String iv) {
-    return new IvParameterSpec(getBytesWithLegalSize(iv));
   }
 }
