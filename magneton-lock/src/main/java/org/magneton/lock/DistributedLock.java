6cc2649abff1605f658d9afb18699f5d986d9857
张明爽 <zms198983@163.com>
2021-01-22 10:45:49 +0800

    feat(lock): add redis distributed lock to spring boot starter.

--- magneton-lock/src/main/java/org/magneton/lock/DistributedLock.java
+++ magneton-lock/src/main/java/org/magneton/lock/DistributedLock.java
@@ -9,17 +9,4 @@ import java.util.concurrent.locks.Lock;
  * @version 1.0.0
  * @since 2021/1/7
  */
-public interface DistributedLock extends Lock {
-
-  String PREFIX = "dl_";
-
-  /**
-   * reoutch key.
-   *
-   * @param key the given lock key.
-   * @return decorated lock key.
-   */
-  default String reoutchKey(String key) {
-    return PREFIX + key;
-  }
-}
+public interface DistributedLock extends Lock {}
--- spring-boot-starter-magneton/src/main/java/org/magneton/properties/MagnetonProperties.java
+++ spring-boot-starter-magneton/src/main/java/org/magneton/properties/MagnetonProperties.java
@@ -1,5 +1,8 @@
 package org.magneton.properties;
 
+import org.magneton.lock.LockProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * .
  *
@@ -7,10 +10,13 @@ package org.magneton.properties;
  * @version 1.0.0
  * @since 2020/12/25
  */
+@ConfigurationProperties(prefix = MagnetonProperties.PREFIX)
 public class MagnetonProperties {
 
   public static final String PREFIX = "magneton";
+  public static final String PREFIX_LOCK = "magneton.lock";
+  /** lock properties. */
+  private LockProperties lock;
 
-  private MagnetonProperties() {
-  }
+  private MagnetonProperties() {}
 }
