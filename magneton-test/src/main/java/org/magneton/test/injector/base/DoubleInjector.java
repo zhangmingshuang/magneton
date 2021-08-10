bf505b386125de3b918175e9cefe1c0b6dbd3d0a
张明爽 <zms198983@163.com>
2021-08-10 09:53:12 +0800

    fix: 生成随机数如果小值与大值一致时问题

--- magneton-test/src/main/java/org/magneton/test/injector/base/DoubleInjector.java
+++ magneton-test/src/main/java/org/magneton/test/injector/base/DoubleInjector.java
@@ -1,13 +1,12 @@
 package org.magneton.test.injector.base;
 
-import java.math.BigDecimal;
-import java.util.concurrent.ThreadLocalRandom;
 import org.magneton.test.annotation.TestComponent;
 import org.magneton.test.core.Config;
 import org.magneton.test.exception.NoSupportTypeCreateException;
 import org.magneton.test.injector.AbstractInjector;
 import org.magneton.test.injector.Inject;
 import org.magneton.test.injector.InjectType;
+import org.magneton.test.util.ConfigUtil;
 
 /**
  * .
@@ -20,10 +19,7 @@ public class DoubleInjector extends AbstractInjector {
 
   @Override
   protected Object createValue(Config config, InjectType injectType, Inject inject) {
-    return BigDecimal.valueOf(
-            ThreadLocalRandom.current().nextDouble(config.getMinDouble(), config.getMaxDouble()))
-        .setScale(config.getDoubleScale(), config.getDoubleRoundingModel())
-        .doubleValue();
+    return ConfigUtil.nextDouble(config);
   }
 
   @Override
--- magneton-test/src/main/java/org/magneton/test/injector/base/ShortInjector.java
+++ magneton-test/src/main/java/org/magneton/test/injector/base/ShortInjector.java
@@ -1,12 +1,12 @@
 package org.magneton.test.injector.base;
 
-import java.util.concurrent.ThreadLocalRandom;
 import org.magneton.test.annotation.TestComponent;
 import org.magneton.test.core.Config;
 import org.magneton.test.exception.NoSupportTypeCreateException;
 import org.magneton.test.injector.AbstractInjector;
 import org.magneton.test.injector.Inject;
 import org.magneton.test.injector.InjectType;
+import org.magneton.test.util.ConfigUtil;
 
 /**
  * .
@@ -19,7 +19,7 @@ public class ShortInjector extends AbstractInjector {
 
   @Override
   protected Object createValue(Config config, InjectType injectType, Inject inject) {
-    return (short) ThreadLocalRandom.current().nextInt(config.getMinShort(), config.getMaxShort());
+    return ConfigUtil.nextShort(config);
   }
 
   @Override
