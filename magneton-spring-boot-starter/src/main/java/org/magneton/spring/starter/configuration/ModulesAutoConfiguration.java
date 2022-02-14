5a9a35103de506ab1510e0d4a46f9afabd594ad2
张明爽 <mingshuang.zhang@nascent.cn>
2022-02-14 14:44:53 +0800

    refactor(*): add geo to spring boot starter.

--- magneton-spring-boot-starter/src/main/java/org/magneton/spring/starter/configuration/ModulesAutoConfiguration.java
+++ magneton-spring-boot-starter/src/main/java/org/magneton/spring/starter/configuration/ModulesAutoConfiguration.java
@@ -4,6 +4,8 @@ import org.magneton.module.distributed.cache.DistributedCache;
 import org.magneton.module.distributed.cache.redis.RedissonDistributedCache;
 import org.magneton.module.distributed.lock.DistributedLock;
 import org.magneton.module.distributed.lock.redis.RedissonDistributedLock;
+import org.magneton.module.geo.Geo;
+import org.magneton.module.geo.redis.RedissonGeo;
 import org.redisson.api.RedissonClient;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -18,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 1.2.0
  */
 @Configuration
-public class DistributedModulesAutoConfiguration {
+public class ModulesAutoConfiguration {
 
 	@Bean
 	@ConditionalOnClass(RedissonClient.class)
@@ -32,6 +34,13 @@ public class DistributedModulesAutoConfiguration {
 	@ConditionalOnMissingBean
 	public DistributedLock distributedLock(RedissonClient redissonClient) {
 		return new RedissonDistributedLock(redissonClient);
+	}
+
+	@Bean
+	@ConditionalOnClass(RedissonClient.class)
+	@ConditionalOnMissingBean
+	public Geo geo(RedissonClient redissonClient) {
+		return new RedissonGeo(redissonClient);
 	}
 
 }
