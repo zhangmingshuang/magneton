c18810c1f7c979cc1030d58eda227e85997c39a7
张明爽 <zms198983@163.com>
2022-03-26 19:09:31 +0800

    oss:test

--- magneton-modules/magneton-module-oss/src/test/java/org/magneton/module/oss/aliyun/AliyunOssTest.java
+++ magneton-modules/magneton-module-oss/src/test/java/org/magneton/module/oss/aliyun/AliyunOssTest.java
@@ -1,10 +1,5 @@
 package org.magneton.module.oss.aliyun;
 
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.auth.sts.AssumeRoleRequest;
-import com.aliyuncs.auth.sts.AssumeRoleResponse;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.MethodType;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -19,44 +14,6 @@ import org.junit.jupiter.api.Test;
  * @since 1.0.0
  */
 class AliyunOssTest {
-
-	@Test
-	void test() throws ClientException {
-		AliyunOssProperty property = new AliyunOssProperty();
-		AliyunOss aliyunOss = new MockAliyunOss(property);
-		DefaultAcsClient client = aliyunOss.getClient();
-		AssumeRoleRequest request = new AssumeRoleRequest();
-		request.setSysMethod(MethodType.POST);
-		request.setRoleArn(property.getRoleArn());
-		request.setRoleSessionName(property.getRoleSessionName());
-		request.setSysRegionId(property.getRegionId());
-		//@formatter:off
-        String policy = "{\n" +
-            "    \"Version\": \"1\", \n" +
-            "    \"Statement\": [\n" +
-            "        {\n" +
-            "            \"Action\": [\n" +
-            "                \"oss:PutObject\"\n" +
-            "            ], \n" +
-            "            \"Resource\": [\n" +
-            "                \"acs:oss:*:*:lookersci/*\" \n" +
-            "            ], \n" +
-            "            \"Effect\": \"Allow\"\n" +
-            "        }\n" +
-            "    ]\n" +
-            "}";
-        //@formatter:on
-		request.setPolicy(policy); // 如果policy为空，则用户将获得该角色下所有权限。
-		request.setDurationSeconds(3600L); // 设置临时访问凭证的有效时间为3600秒。
-		AssumeRoleResponse response = client.getAcsResponse(request);
-		Assertions.assertNotNull(response.getCredentials());
-
-		System.out.println("Expiration: " + response.getCredentials().getExpiration());
-		System.out.println("Access Key Id: " + response.getCredentials().getAccessKeyId());
-		System.out.println("Access Key Secret: " + response.getCredentials().getAccessKeySecret());
-		System.out.println("Security Token: " + response.getCredentials().getSecurityToken());
-		System.out.println("RequestId: " + response.getRequestId());
-	}
 
 	@Test
 	void testTime() {
@@ -81,21 +38,16 @@ class AliyunOssTest {
 	@Test
 	void putObject() {
 		AliyunOssProperty property = new AliyunOssProperty();
-		property.setAccessKey("LTAI5t7pQ6X5ikNdG28J7NjS");
-		property.setAccessKeySecret("owUAxuLKuYzuArT7QTYCImAtyasaoz");
-		property.setEndpoint("oss-cn-hangzhou.aliyuncs.com");
-		property.setRoleArn("acs:ram::1199947484795824:role/ramossrole");
-		property.setRoleSessionName("test");
-		property.setRegionId("cn-hangzhou");
-		property.setDefaultBucket("lookersci");
-		AliyunOss aliyunOss = new AliyunOss(property);
+		property.setDefaultBucket("defaultBucket");
+		AliyunOss aliyunOss = new MockAliyunOss(property);
 		AliyunStsRes sts = aliyunOss.sts(null);
 
 		System.out.println("======== sts ==========");
 		System.out.println(sts);
 		System.out.println("======== sts ==========");
 
-		aliyunOss.simpleUpdate(sts, "/abc/aaa/test.png", Paths.get("D://1.png").toFile());
+		Assertions.assertThrows(Exception.class,
+				() -> aliyunOss.simpleUpdate(sts, "/abc/aaa/test.png", Paths.get("D://1.png").toFile()));
 	}
 
 }
