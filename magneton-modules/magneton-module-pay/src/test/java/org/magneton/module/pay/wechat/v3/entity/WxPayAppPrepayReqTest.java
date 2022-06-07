ffea05bd5109f5047d87162ab4c6f905670a8144
张明爽 <zms198983@163.com>
2022-06-07 23:00:16 +0800

    feat(pay): 单元测试

--- magneton-modules/magneton-module-pay/src/test/java/org/magneton/module/pay/wechat/v3/entity/WxPayAppPrepayReqTest.java
+++ magneton-modules/magneton-module-pay/src/test/java/org/magneton/module/pay/wechat/v3/entity/WxPayAppPrepayReqTest.java
@@ -3,7 +3,7 @@ package org.magneton.module.pay.wechat.v3.entity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.magneton.module.pay.wechat.v3.prepay.WechatBaseV3Pay;
+import org.magneton.module.pay.wechat.v3.core.BaseV3Api;
 import org.magneton.module.pay.wechat.v3.prepay.entity.WxPayAppPrepayReq;
 import org.magneton.test.ChaosTest;
 
@@ -16,16 +16,16 @@ class WxPayAppPrepayReqTest {
 	@Test
 	void test() throws JsonProcessingException {
 		WxPayAppPrepayReq req = ChaosTest.createExcepted(WxPayAppPrepayReq.class);
-		String json = WechatBaseV3Pay.json().writeValueAsString(req);
+		String json = BaseV3Api.json().writeValueAsString(req);
 		Assertions.assertTrue(json.contains("appid") && json.contains("mchid"));
 
-		WxPayAppPrepayReq jsonReq = WechatBaseV3Pay.json().readValue(
+		WxPayAppPrepayReq jsonReq = BaseV3Api.json().readValue(
 				"{\n" + "\t\"mchid\": \"1900006XXX\",\n" + "\t\"out_trade_no\": \"APP1217752501201407033233368018\",\n"
 						+ "\t\"appid\": \"wxb4ba3c02aa476XXX\",\n" + "\t\"description\": \"Image形象店-深圳腾大-QQ公仔\",\n"
 						+ "\t\"notify_url\": \"https://weixin.qq.com/\",\n" + "\t\"amount\": {\n"
 						+ "\t\t\"total\": 1,\n" + "\t\t\"currency\": \"CNY\"\n" + "\t}\n" + "}",
 				WxPayAppPrepayReq.class);
-		System.out.println(WechatBaseV3Pay.json().writeValueAsString(jsonReq));
+		System.out.println(BaseV3Api.json().writeValueAsString(jsonReq));
 	}
 
 }
