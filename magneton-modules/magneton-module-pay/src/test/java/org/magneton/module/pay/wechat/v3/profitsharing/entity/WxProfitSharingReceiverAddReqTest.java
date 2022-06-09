9f4b98a177fefb616de00492503e7adaab2f79b8
张明爽 <zms198983@163.com>
2022-06-09 22:43:43 +0800

    feat(pay): wechat

---
+++ magneton-modules/magneton-module-pay/src/test/java/org/magneton/module/pay/wechat/v3/profitsharing/entity/WxProfitSharingReceiverAddReqTest.java
@@ -0,0 +1,23 @@
+package org.magneton.module.pay.wechat.v3.profitsharing.entity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.magneton.module.pay.wechat.v3.core.BaseV3Api;
+import org.magneton.module.pay.wechat.v3.core.WxReceiverType;
+
+/**
+ * @author zhangmsh 2022/6/9
+ * @since 1.0.1
+ */
+class WxProfitSharingReceiverAddReqTest {
+
+	@Test
+	void test() throws JsonProcessingException {
+		WxProfitSharingReceiverAddReq req = new WxProfitSharingReceiverAddReq();
+		req.setType(WxReceiverType.MERCHANT_ID);
+		String json = BaseV3Api.json().writeValueAsString(req);
+		Assertions.assertEquals("{\"type\":\"MERCHANT_ID\"}", json);
+	}
+
+}
