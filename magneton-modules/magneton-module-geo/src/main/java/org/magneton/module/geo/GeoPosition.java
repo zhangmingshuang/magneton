4b001c1e53697bb5138c741f75ca89e871d52bbd
zhangmsh <mingshuang.zhang@nascent.cn>
2024-03-09 16:52:25 +0800

    test(*): pass test

--- magneton-modules/magneton-module-geo/src/main/java/org/magneton/module/geo/GeoPosition.java
+++ magneton-modules/magneton-module-geo/src/main/java/org/magneton/module/geo/GeoPosition.java
@@ -19,6 +19,8 @@ import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 /**
  * @author Nikita Koksharov
  *
@@ -69,13 +71,10 @@ public class GeoPosition {
 			return false;
 		}
 		GeoPosition other = (GeoPosition) obj;
-		if (Double.doubleToLongBits(this.latitude) != Double.doubleToLongBits(other.latitude)) {
+		if (!Objects.equals(Double.doubleToLongBits(this.latitude), Double.doubleToLongBits(other.latitude))) {
 			return false;
 		}
-		if (Double.doubleToLongBits(this.longitude) != Double.doubleToLongBits(other.longitude)) {
-			return false;
-		}
-		return true;
+		return Objects.equals(Double.doubleToLongBits(this.longitude), Double.doubleToLongBits(other.longitude));
 	}
 
 	@Override
