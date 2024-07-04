c7234c2f91a1fbf2873bcdb0e178b0ab83cb0e21
张明爽 <zms198983@163.com>
2024-07-04 10:07:55 +0800

    1

--- magneton-spring-boot-starter/src/main/java/org/magneton/spring/starter/MagnetonAutoConfiguration.java
+++ magneton-spring-boot-starter/src/main/java/org/magneton/spring/starter/MagnetonAutoConfiguration.java
@@ -1,12 +1,7 @@
 package org.magneton.spring.starter;
 
-import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
-import org.magneton.spring.starter.core.PropertyDesResolver;
 import org.magneton.spring.starter.properties.MagnetonProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,23 +21,23 @@ public class MagnetonAutoConfiguration {
 	// return RunLauncherContextInitializer.getInstance();
 	// }
 
-	/**
-	 * 配置信息解码处理器
-	 * @return EncryptablePropertyResolver
-	 * @since zhongjinbin 20210916
-	 */
-	@ConditionalOnMissingBean
-	@ConditionalOnClass(EncryptablePropertyResolver.class)
-	@Bean(name = "encryptablePropertyResolver")
-	public EncryptablePropertyResolver encryptablePropertyResolver() {
-		return new EncryptablePropertyResolver() {
-			private final PropertyDesResolver propertyDesResolver = new PropertyDesResolver();
-
-			@Override
-			public String resolvePropertyValue(String s) {
-				return this.propertyDesResolver.resolvePropertyValue(s);
-			}
-		};
-	}
+	// /**
+	// * 配置信息解码处理器
+	// * @return EncryptablePropertyResolver
+	// * @since zhongjinbin 20210916
+	// */
+	// @ConditionalOnMissingBean
+	// @ConditionalOnClass(EncryptablePropertyResolver.class)
+	// @Bean(name = "encryptablePropertyResolver")
+	// public EncryptablePropertyResolver encryptablePropertyResolver() {
+	// return new EncryptablePropertyResolver() {
+	// private final PropertyDesResolver propertyDesResolver = new PropertyDesResolver();
+	//
+	// @Override
+	// public String resolvePropertyValue(String s) {
+	// return this.propertyDesResolver.resolvePropertyValue(s);
+	// }
+	// };
+	// }
 
 }
